666396a2ceed6e0960823e2ec03376ea4eb87b75
bijin-gho <bijin@gho.care>
2025-10-01 11:45:42 +0530

    add color and fonts

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+
+  static const Color primarycolor = Color(0xff667EFA);
+  static const Color secondarycolor = Color.fromARGB(255, 132, 146, 213);
+  static const Color backgroundcolor = Color.fromRGBO(244, 245, 247, 1);
+  static const Color textcolor = Color(0xff404040);
+  
+
+  
+
+}
