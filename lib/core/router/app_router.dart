98a65104c2e2dae0de9e948a45bb2d5fd7fb9154
swathi <swathi@gho.care>
2025-10-02 14:56:01 +0530

    update resend otp

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ghorx_mobile_app_new/features/auth/otp_screen.dart';
 import 'package:ghorx_mobile_app_new/features/auth/register_screen.dart';
+import 'package:ghorx_mobile_app_new/features/auth/repository/model/login_model.dart';
 import 'package:ghorx_mobile_app_new/features/auth/reset_password.dart';
 import '../../features/auth/login_screen.dart';
 import '../../features/splash/splash_screen.dart';
@@ -17,15 +18,19 @@ class AppRouter {
       case splash:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       case login:
-        return MaterialPageRoute(builder: (_) =>  LoginScreen());
-      
+        return MaterialPageRoute(builder: (_) => LoginScreen());
+
       case register:
         return MaterialPageRoute(builder: (_) => RegisterScreen());
       case otp:
-        return MaterialPageRoute(builder: (_) => OtpScreen());
-        case reset:
-        return MaterialPageRoute(builder: (_) =>  ResetPasswordScreen());
-  
+        final otpResponse = settings.arguments as OtpResponse;
+        return MaterialPageRoute(
+          builder: (_) => OtpScreen(otpResponse: otpResponse),
+          settings: settings,
+        );
+      case reset:
+        return MaterialPageRoute(builder: (_) => ResetPasswordScreen());
+
       default:
         return MaterialPageRoute(
           builder:
