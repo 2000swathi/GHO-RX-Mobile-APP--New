a0b179e0efe37b5c168cc2d7207be353c59ce3a3
sourav-jc <sourav@gho.care>
2025-10-02 15:14:38 +0530

    update

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ghorx_mobile_app_new/features/auth/otp_screen.dart';
 import 'package:ghorx_mobile_app_new/features/auth/register_screen.dart';
-<<<<<<< HEAD
-import 'package:ghorx_mobile_app_new/features/home/home_scrn.dart';
-=======
 import 'package:ghorx_mobile_app_new/features/auth/repository/model/login_model.dart';
-import 'package:ghorx_mobile_app_new/features/auth/reset_password.dart';
->>>>>>> origin/develop
+import 'package:ghorx_mobile_app_new/features/home/add_page.dart';
 import '../../features/auth/login_screen.dart';
 import '../../features/splash/splash_screen.dart';
 
@@ -15,11 +11,7 @@ class AppRouter {
   static const String login = '/login';
   static const String register = '/register';
   static const String otp = '/otp';
-<<<<<<< HEAD
-  static const String home = '/home';
-=======
-  static const String reset = '/reset';
->>>>>>> origin/develop
+  static const String addPage = '/addPage';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -27,23 +19,16 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       case login:
         return MaterialPageRoute(builder: (_) => LoginScreen());
-
       case register:
         return MaterialPageRoute(builder: (_) => RegisterScreen());
       case otp:
-<<<<<<< HEAD
-        return MaterialPageRoute(builder: (_) => OtpScreen());
-      case home:
-        return MaterialPageRoute(builder: (_) => HomeScrn());
-=======
         final otpResponse = settings.arguments as OtpResponse;
         return MaterialPageRoute(
           builder: (_) => OtpScreen(otpResponse: otpResponse),
           settings: settings,
         );
-      case reset:
-        return MaterialPageRoute(builder: (_) => ResetPasswordScreen());
->>>>>>> origin/develop
+      case addPage:
+        return MaterialPageRoute(builder: (_) => AddPage());
 
       default:
         return MaterialPageRoute(
