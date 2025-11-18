c70f693aadb64ef736cc7678993aa5c67f0070b7
sourav-jc <sourav@gho.care>
2025-11-18 10:11:15 +0530

    profile ui new

--- lib/features/account/account_page.dart
+++ lib/features/account/account_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ghorx_mobile_app_new/core/constants/app_colors.dart';
 import 'package:ghorx_mobile_app_new/core/constants/app_fonts.dart';
+import 'package:ghorx_mobile_app_new/features/account/widget/common_container.dart';
 import 'package:ghorx_mobile_app_new/features/authentication/cases/cases_pages/widgets/case_appbar.dart';
 
 class AccountPage extends StatelessWidget {
@@ -11,32 +12,34 @@ class AccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CaseAppBar(title: "Account", color: AppColors.backgroundcolor),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           children: [
             Column(
               children: [
-                Stack(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: AppColors.glasscontainer,
-                      radius: 47,
-                    ),
+                Center(
+                  child: Stack(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: AppColors.glasscontainer,
+                        radius: 47,
+                      ),
 
-                    Positioned(
-                      right: 0,
-                      bottom: 0,
-                      child: CircleAvatar(
-                        radius: 16,
-                        backgroundColor: AppColors.white,
-                        child: SvgPicture.asset(
-                          "assets/svg/account/edit.svg",
-                          height: 16,
-                          width: 16,
+                      Positioned(
+                        right: 0,
+                        bottom: 0,
+                        child: CircleAvatar(
+                          radius: 16,
+                          backgroundColor: AppColors.white,
+                          child: SvgPicture.asset(
+                            "assets/svg/account/edit.svg",
+                            height: 16,
+                            width: 16,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
                 SizedBox(height: 5),
                 Text("data", style: AppFonts.heading),
@@ -44,7 +47,92 @@ class AccountPage extends StatelessWidget {
               ],
             ),
             ///////////////////////
-            Text("Manage Account"),
+            Padding(
+              padding: const EdgeInsets.only(left: 14, right: 14),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 49),
+                  Text(
+                    "Manage Account",
+                    style: AppFonts.header_5.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  commonInfoContainer(
+                    title: "Profile Information",
+                    subTitle: "Manage your account details",
+                    svgPath: "assets/svg/account/accperson.svg",
+                  ),
+                  SizedBox(height: 36),
+                  Text(
+                    "Security & Privacy",
+                    style: AppFonts.header_5.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  commonInfoContainer(
+                    title: "Change Password",
+                    subTitle: "Reset your password anytime for safety.",
+                    svgPath: "assets/svg/account/lock.svg",
+                  ),
+                  commonInfoContainer(
+                    title: "Privacy Policy",
+                    subTitle: "Review our data protection practices.",
+                    svgPath: "assets/svg/account/locky.svg",
+                  ),
+                  commonInfoContainer(
+                    title: "Microphone Access",
+                    subTitle: "Allow app to use microphone.",
+                    svgPath: "assets/svg/account/mic.svg",
+                  ),
+                  commonInfoContainer(
+                    title: "Notifications",
+                    subTitle: "Allow app to send alerts.",
+                    svgPath: "assets/svg/account/bell.svg",
+                  ),
+                  SizedBox(height: 36),
+                  Text(
+                    "Account Control",
+                    style: AppFonts.header_5.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  commonInfoContainer(
+                    title: "Logout",
+                    subTitle: "Sign out from your account safely.",
+                    svgPath: "assets/svg/account/logout.svg",
+                  ),
+                  commonInfoContainer(
+                    title: "Delete Account",
+                    subTitle: "Close your account permanently.",
+                    svgPath: "assets/svg/account/del.svg",
+                  ),
+                  SizedBox(height: 36),
+                  Text(
+                    "Information & Support",
+                    style: AppFonts.header_5.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  commonInfoContainer(
+                    title: "About",
+                    subTitle: "View app details.",
+                    svgPath: "assets/svg/account/about.svg",
+                  ),
+                  commonInfoContainer(
+                    title: "Help",
+                    subTitle: "Contact us via email.",
+                    svgPath: "assets/svg/account/helpp.svg",
+                  ),
+                  SizedBox(height: 50),
+                ],
+              ),
+            ),
           ],
         ),
       ),
