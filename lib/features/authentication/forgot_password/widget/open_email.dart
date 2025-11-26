59c5992454276814d8eb7b9ef792f2308c624834
mdrinshad <rinshad@gho.care>
2025-11-26 15:38:11 +0530

    updated

--- lib/features/authentication/forgot_password/widget/open_email.dart
+++ lib/features/authentication/forgot_password/widget/open_email.dart
@@ -1,67 +1,39 @@
 import 'dart:io';
-import 'package:android_intent_plus/android_intent.dart';
+import 'package:flutter/foundation.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> openEmailInbox() async {
-  if (Platform.isAndroid) {
-    // OPEN GMAIL APP SAFE (DOES NOT CLOSE YOUR APP)
-    const gmailHome = AndroidIntent(
-      action: 'android.intent.action.MAIN',
-      category: 'android.intent.category.APP_EMAIL',
-      package: 'com.google.android.gm',
-      flags: <int>[
-        0x10000000, // FLAG_ACTIVITY_NEW_TASK
-      ],
-    );
-    try {
-      await gmailHome.launch();
-      return;
-    } catch (_) {}
-
-    // Gmail inbox intent
-    const gmailInboxIntent = AndroidIntent(
-      action: 'android.intent.action.VIEW',
-      package: 'com.google.android.gm',
-      data: 'content://com.google.android.gm.inbox',
-      flags: <int>[
-        0x10000000, // FLAG_ACTIVITY_NEW_TASK
-      ],
-    );
-    try {
-      await gmailInboxIntent.launch();
-      return;
-    } catch (_) {}
-
-    // Email chooser
-    final emailUri = Uri.parse("mailto:");
-    if (await canLaunchUrl(emailUri)) {
+  try {
+    // ---------------- ANDROID ----------------
+    if (!kIsWeb && Platform.isAndroid) {
+      final Uri emailUri = Uri.parse("mailto:");
       await launchUrl(emailUri, mode: LaunchMode.externalApplication);
       return;
     }
 
-    // Web fallback
-    final gmailWeb = Uri.parse("https://mail.google.com/mail/u/0/#inbox");
-    await launchUrl(gmailWeb, mode: LaunchMode.inAppBrowserView);
-    return;
-  }
+    // ---------------- iOS ----------------
+    if (!kIsWeb && Platform.isIOS) {
+      final gmailInbox = Uri.parse("googlegmail://");
+      if (await canLaunchUrl(gmailInbox)) {
+        await launchUrl(gmailInbox);
+        return;
+      }
 
-  // ----------------------------
-  // iOS
-  // ----------------------------
-  if (Platform.isIOS) {
-    final gmailInbox = Uri.parse("googlegmail://inbox");
-    if (await canLaunchUrl(gmailInbox)) {
-      await launchUrl(gmailInbox);
+      final appleMail = Uri.parse("message://");
+      if (await canLaunchUrl(appleMail)) {
+        await launchUrl(appleMail);
+        return;
+      }
+
+      final gmailWeb = Uri.parse("https://mail.google.com");
+      await launchUrl(gmailWeb, mode: LaunchMode.externalApplication);
       return;
     }
 
-    final appleMail = Uri.parse("message://");
-    if (await canLaunchUrl(appleMail)) {
-      await launchUrl(appleMail);
-      return;
-    }
-
-    final gmailWeb = Uri.parse("https://mail.google.com/mail/u/0/#inbox");
+    // ---------------- WEB FALLBACK ----------------
+    final gmailWeb = Uri.parse("https://mail.google.com");
     await launchUrl(gmailWeb, mode: LaunchMode.externalApplication);
+  } catch (e) {
+    debugPrint("Error opening email: $e");
   }
 }
